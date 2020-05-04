078baac2dc9f4bcbc31d4da19f9379b2e8da82c0
Буй Дык Туан <62297741+TuanBuiDucVolsu@users.noreply.github.com>
2020-05-04 10:53:02 +0400

    Update tsum.c

--- tsum.c
+++ tsum.c
@@ -3,7 +3,7 @@ double tsum(double a,int t)
 {
     int i,j;
     double sum = 1-a;
-	if(t == 1)
+	if(t == 1)                                 //особый случай
 	{
 		sum = sum ;
 		printf("Sum : %lf \n",sum);
@@ -13,11 +13,11 @@ double tsum(double a,int t)
 	        double t = 1;	             //t - числитель
 		for(i = 2 ; i <= t ; i++)
 		{
-		t = pow(-1,i)*pow(a,2.0*i-1) ;
+		t = pow(-1,i)*pow(a,2.0*i-1) ;          //Рассчитать значение числителя
 	        int m = 1;                          //m- знаменатель
 		for(j = 2 ; j <= i ; j++)
 		{
-			m *= ((2*j - 2) * (2*j - 1));
+			m *= ((2*j - 2) * (2*j - 1));     //Рассчитать значение знаменателя
 		}
 		sum += t/m;
                 }
