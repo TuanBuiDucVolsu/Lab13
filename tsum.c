0eb3e73299e46856a01694d4434bfa613420d649
Буй Дык Туан <62297741+TuanBuiDucVolsu@users.noreply.github.com>
2020-05-04 10:45:49 +0400

    Update tsum.c

--- tsum.c
+++ tsum.c
@@ -1,27 +1,27 @@
 #include "tsum.h"
 double tsum(double a,int t)
 {
-	int i,j;
+    int i,j;
     double sum = 1-a;
 	if(t == 1)
 	{
 		sum = sum ;
-		printf("tong la : %lf \n",sum);
+		printf("Sum : %lf \n",sum);
 	}
 	else
 	{
-	        double tu = 1;	
+	        double t = 1;	             //t - числитель
 		for(i = 2 ; i <= t ; i++)
 		{
-		tu = pow(-1,i)*pow(a,2.0*i-1) ;
-	    int mau = 1;
+		t = pow(-1,i)*pow(a,2.0*i-1) ;
+	        int m = 1;                          //m- знаменатель
 		for(j = 2 ; j <= i ; j++)
 		{
-			mau *= ((2*j - 2) * (2*j - 1));
+			m *= ((2*j - 2) * (2*j - 1));
 		}
-		sum += tu/mau;
+		sum += t/m;
                 }
-        printf("tong la: %lf \n",sum);
+        printf("SUm: %lf \n",sum);
         }
 	return sum;
 }
